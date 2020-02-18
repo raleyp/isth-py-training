d34b63e30ef95ddacb5d0d3318fae0b739160960
Marny Lopez - AKC MAC <marny@local.akc.org>
2020-02-18 14:20:30 -0600

    create user and call methods

--- app/run.py
+++ app/run.py
@@ -2,3 +2,11 @@ from adaptor import file_routes as file
 from domain.model import Usuario
 
 print('Iniciando')
+
+
+usuario = Usuario()
+usuario.Username = 'marnylopez'
+usuario.Nombre = 'Marny'
+usuario.Apellido = 'Lopez'
+
+file.create_file('archivo', usuario.provide())
