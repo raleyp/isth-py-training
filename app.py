dc51ded5f35faa9fc4595a2355bfade3947978bc
raleyp <61215140+raleyp@users.noreply.github.com>
2020-03-10 14:10:24 -0600

    Update app.py
    Se crea función Sumar que recibe dos valores y devuelve el resultado de la suma de ambos. Roberto Aley.

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-def sumar (v1,v2):
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def sumar ():
+    v1 = int(request.args.get("v1", "v1")) 
+    v2 = int(request.args.get("v2", "v2")) 
     suma = v1 + v2
-    return suma
+    return f'La suma es: {escape(suma)}'
